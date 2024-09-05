bd24f75a39dc988d87f9b266596830d61692d732
maharjankee <krishamaharjan264@gmail.com>
2024-09-05 07:45:00 +0545

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <> <h1>Navbar</h1>
+      <p>this is navbar.</p>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </>
+    
+  )
+}
+
+export default Navbar
